79cb3c514a508b26bef1b9b795288280c378ea3d
Daniel Dihardja <daniel@dihardja.de>
2019-08-28 11:35:11 +0200

    added upload to s3

--- app.js
+++ app.js
@@ -2,11 +2,40 @@
  * Created by danieldihardja on 04.08.19.
  */
 
-const io = require('socket.io')(process.env.PORT || 8080);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('data', (data) => {
-    console.log(data);
+const aws = require('aws-sdk');
+const cors = require('cors');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+const express = require('express');
+const app = express();
+const port = process.env.port || 5555;
+
+app.use(cors());
+app.use(express.static('images'));
+
+app.get('/', (req, res) => {
+  res.send('sinus duktus');
+});
+
+const s3 = new aws.S3();
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'sinusduktus/images',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + '.png')
+    }
   })
 });
+
+//const upload = multer({ storage: storage });
+app.post('/images', upload.single('sd-image'), (req, res) => {
+  res.send(req.file);
+});
+
+app.listen(port, () => console.log(`running on port ${port}`));
